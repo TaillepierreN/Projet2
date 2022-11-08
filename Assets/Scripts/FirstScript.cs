a15d57091cd383571f4854ff4676f2a0b05ffe22
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2022-11-08 11:36:16 +0100

    mardi

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,18 +10,138 @@ public class FirstScript : MonoBehaviour
     public int nombre;
 
     //private = accessible seulement dans ce script
-    private int nombre2;
+    private int nombre2 = 50;
     
     // est private par defaut
-    int nombre3;
+    int nombre3 = 69;
+    int nombre4 = 5;
 
+    public int somme;
     public string chaine;
+    public string chaine1 = "et puis la suite";
+    public int age;
+    public string job;
+    bool bac;
+    
+    
 
     // Start is called before the first frame update
-    void Start()
+     void Start()
     {
-        nombre = 15;
+        nombre = 15 + 15;
         chaine = "une phrase dedans";
+        print("affichage console");
+        print(nombre2);
+        somme = nombre3 + nombre4;
+        //concatenation
+        print("la somme " + somme);
+        print("voila " + chaine + " " + chaine1);
+        print($"hello c'est une phrase avec {chaine}");
+
+        // incrementation
+        nombre = nombre+1;
+        
+        // condition
+        nombre = 15;
+        if(nombre>15)
+        {
+            print("nombre supérieur à 15");
+        } else if (nombre < 15)
+        {
+            print("nombre inférieur à 15");
+            
+        } 
+        else
+        {
+            print("nombre égal à 15");
+        };
+        
+        //exo
+        age = 18;
+        if(age>12 && age < 17)
+        {
+            print("vous êtes un ado");
+        } else if (age <= 12)
+        {
+            print("Vous etes un enfant");
+        } else {
+            print("vous etes un adulte normalement");
+        };
+        
+        //job = "gamer";
+        //job = "dev";
+        job = "baker";
+        if(job == "gamer" || job == "dev")
+        {
+            print("vous pouvez passer");
+        } else 
+        {
+            print("You Shall Not Pass!");
+        }
+
+        //Boucle
+
+        //tant que
+        /*
+        while(nombre<=100)
+        {
+            print("nombre = " + nombre);
+            nombre ++;
+        }
+        */
+
+        //Pour
+        /*
+        for(int i=1; i<100; i++)
+        {
+            print("nombre i = " + i);
+
+        }
+        */
+
+        //fait tant que , execute au moins une fois
+        /*
+        do{;
+            print("nombre2 = " + nombre2);
+            nombre2++;
+        }while(nombre2 <=100);
+        */
+
+        //exo
+
+        /*
+        enfant de 3ans va a l'ecole jusqu'as 18ans,
+        si il a le bac il va a la fac sinon redouble une fois.
+        après 5 ans de fac il devient métier
+        */
+        int ageChild;
+        for(ageChild = 3; ageChild<=17; ageChild++)
+        {
+            print("l'enfant à " + ageChild + "ans");
+        }
+        do{
+            print("il passe son bac");
+            int rdn = Random.Range(0, 2);
+            ageChild++;
+            if (rdn==1)
+            {
+                print("il rate son bac");
+            } else
+            {
+                print("il a son bac");
+                bac = true;
+            }
+        }while(!bac);
+        for (int i=0; i<6; i++)
+        {
+            print("il va a la fac");
+            ageChild++;
+        }
+        print("il trouve un metier à " +ageChild + "ans");
+        
+
+
+
         
     }
 
---
+++ Assets/Scripts/autreExo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autreExo : MonoBehaviour
+{
+    public int[] monTableau = new int[5];
+    public int[] monTableau1 = new int[]{12,15,15,61};
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(monTableau[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
