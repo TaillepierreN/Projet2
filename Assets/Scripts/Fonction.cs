ae6f3bc32a4a75237ae5267ea3c3248a0d9e8b67
Nicolas Taillepierre <taillepierren@gmail.com>
2022-11-10 11:37:27 +0100

    remplissage fonction

--- Assets/Scripts/Fonction.cs
+++ Assets/Scripts/Fonction.cs
@@ -2,18 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fonction : MonoBehaviour
+public class lesfonctions : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int nombre;
+    public string nprenom;
+
     void Start()
     {
-        
+        nombre = maFonction(89);       
+        print("nombre " + nombre);
+        nprenom = maString("Olivier");
+        print("pstart " + nprenom);
+        affichage("Corinne",24);
+        affichage(nprenom,39);
     }
 
-    // Update is called once per frame
-    void Update()
+#region mafonction
+    public int maFonction(int nchiffre)
     {
+        return nchiffre*5;
+    }
+#endregion
+    public string maString(string nom)
+    {
+        return nom;
+    }
+
+    public void affichage(string prenom, int age)
+    {
+        print("prenom : " + prenom + " j'ai " + age + "ans" );
         
     }
-    
 }
